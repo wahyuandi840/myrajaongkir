3751a083d54e4624d080491e56919d8de1355345
wahyuandi840 <wahyuandi840@gmail.com>
2018-03-08 11:24:41 +0700

    Create MyRajaOngkir.php

---
+++ src/MyRajaOngkir.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MyRajaOngkir;
+
+use MyRajaOngkir\App\Provinsi;
+use MyRajaOngkir\App\Kota;
+use MyRajaOngkir\App\Cost;
+
+class MyRajaOngkir {
+
+	/* Singleton */
+	protected static function GetInstance(){
+		if( self::$obj == null ){
+			self::$obj = new RajaOngkir;
+		}
+
+		return self::$obj;
+	}
+	protected static $obj = null;
+	/* End Singleton */
+
+	public static function __callStatic($name, $arguments){
+		self::GetInstance()->__call($name, $arguments);
+	}
+
+	public function Provinsi(){
+		return new Provinsi;
+	}
+
+	public function Kota(){
+		return new Kota;
+	}
+
+	public function Cost($attributes){
+		return new Cost($attributes);
+	}
+
+}
